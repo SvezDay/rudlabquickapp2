0bb9cb14dd52c03ffbdf0352540a979f7d377e1c
SvezDay <svez.day@gmail.com>
2018-10-11 16:08:00 +0200

    modification des parametres de compilations

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ const favicon = require('serve-favicon');
 
 const app = express();
 
-app.use(express.static(pj(__dirname, '/dist')));
+app.use(express.static(__dirname + '/dist'));
 // app.use(favicon('/favicon.ico'));
 
-app.get('/*', (req, res)=>{
+app.get('*', (req, res)=>{
   res.sendFile(pj(__dirname, '/dist/index.html'));
 });
 
-const port = process.env.PORT || 4200;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, ()=>{console.log('Running on the port 4200')});
+app.listen(process.env.PORT || 4200);
+// const port = process.env.PORT || 4200;
+// app.set('port', port);
+//
+// const server = http.createServer(app);
+// server.listen(port, ()=>{console.log('Running on the port 4200')});
